e143dcbfb4bc6d8acad4bd2b23b174d7425bc7db
anovni <153435050+xPloits3c@users.noreply.github.com>
2023-12-19 18:44:14 +0100

    re_volt

--- revolt.sh
+++ revolt.sh
@@ -34,7 +34,7 @@ echo  "[i] NR.T00LS : 30                           ||V . V||" | lolcat
 echo  "[i] VERSION  : 1.0                          ||_.-._||" | lolcat
 echo  "╭── 📁The Installed tools are in /Revolt    °--)-(--°" | lolcat
 echo  "|── 🌐https://github.com/xPloits3c/Revolt  __[===-o]___" |lolcat
-echo  "╰── 🌐https://twitter.com/AnonSecIta      |__R3VCLT___)| $green " |lolcat
+echo  "╰── 🌐https://twitter.com/ANOVNI1         |__R3VCLT___)| $green " |lolcat
 echo  "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°" | lolcat
 
 
